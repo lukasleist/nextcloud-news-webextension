88178cf31655a7492bf88057abe7033b3b96fc57
Jeffrey Koehler <deef551@gmail.com>
2021-09-09 23:41:35 -0500

    Added function to update localizations on load
    As html files are not scanned for localization, it is necessary to load the text in via js.
    fixes lukasleist/news-chrome-notifier#1

--- popup.js
+++ popup.js
@@ -1,74 +1,91 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("reload-button").addEventListener("click", event => {
-        triggerUpdate();
-    });
-    document.getElementById("read-all-button").addEventListener("click", event => {
-        document.getElementById("articles").innerHTML = "";
-        markAsRead();
-    })
-    updateUi()
-    chrome.runtime.onMessage.addListener(({command}) => {
-        if(command == "updateUi") {
-            updateUi();
-        }
-    })
+	document.getElementById("reload-button").addEventListener("click", event => {
+		triggerUpdate();
+	});
+	document.getElementById("read-all-button").addEventListener("click", event => {
+		document.getElementById("articles").innerHTML = "";
+		markAsRead();
+	})
+	updatePopupLocalizations()
+	updateUi()
+	chrome.runtime.onMessage.addListener(({ command }) => {
+		if (command == "updateUi") {
+			updateUi();
+		}
+	})
 });
 
 function updateUi() {
-    chrome.storage.local.get(["url", "authorization", "unreadCount", "unreadArticles"], ({
-        url, authorization, unreadCount, unreadArticles
-    }) => {
-        let linkElement = document.getElementById("nextcloud-link")
+	chrome.storage.local.get(["url", "authorization", "unreadCount", "unreadArticles"], ({
+		url, authorization, unreadCount, unreadArticles
+	}) => {
+		let linkElement = document.getElementById("nextcloud-link")
 
-        if (authorization && url != null) {
-            linkElement.innerHTML = chrome.i18n.getMessage("openNextcloudNews");
-            linkElement.setAttribute("href", url + "/index.php/apps/news/");
+		if (authorization && url != null) {
+			linkElement.innerHTML = chrome.i18n.getMessage("openNextcloudNews");
+			linkElement.setAttribute("href", url + "/index.php/apps/news/");
 
-            document.getElementById("unread-count").innerHTML = chrome.i18n.getMessage("unreadArticlesHeading", [
-                (unreadCount) ? unreadCount : 0
-            ]);
-            if(unreadArticles) {
-                let ul = document.getElementById("articles");
-                ul.innerHTML = "";
-                for (let article of unreadArticles) {
-                    ul.appendChild(renderArticle(article));
-                }
-            }
-        } else {
-            linkElement.innerHTML = chrome.i18n.getMessage("notAuthenticated");
-            linkElement.setAttribute("href", "/config.html");
-        }
-    });
+			document.getElementById("unread-count").innerHTML = chrome.i18n.getMessage("unreadArticlesHeading", [
+				(unreadCount) ? unreadCount : 0
+			]);
+			if (unreadArticles) {
+				let ul = document.getElementById("articles");
+				ul.innerHTML = "";
+				for (let article of unreadArticles) {
+					ul.appendChild(renderArticle(article));
+				}
+			}
+		} else {
+			linkElement.innerHTML = chrome.i18n.getMessage("notAuthenticated");
+			linkElement.setAttribute("href", "/config.html");
+		}
+	});
 }
 
 function renderArticle(article) {
-    let template = document.createElement("template");
-    template.innerHTML = `
+	let template = document.createElement("template");
+	template.innerHTML = `
         <li>
             <div class="article">
                 <a href="${article.url}" class="title">
                     ${article.title}
                 </a>
-                <button title="Mark as read" class="button read-button"></button>
+                <button title="${chrome.i18n.getMessage("markAsReadTitle")}" class="button read-button"></button>
             </div>
         </li>
     `;
-    let a = template.content.querySelector(".title");
-    a.addEventListener("click", _ => {
-        chrome.tabs.create({ url: article.url, active: false });
-        markAsRead(article.id);
-    });
+	let a = template.content.querySelector(".title");
+	a.addEventListener("click", _ => {
+		chrome.tabs.create({ url: article.url, active: false });
+		markAsRead(article.id);
+	});
 
-    let readButton = template.content.querySelector(".read-button");
-    readButton.addEventListener("click", _ => markAsRead(article.id));
+	let readButton = template.content.querySelector(".read-button");
+	readButton.addEventListener("click", _ => markAsRead(article.id));
 
-    return template.content;
+	return template.content;
 }
 
 function markAsRead(articleIds) {
-    chrome.runtime.sendMessage({command: "markAsRead", articleIds: articleIds});
+	chrome.runtime.sendMessage({ command: "markAsRead", articleIds: articleIds });
 }
 
 function triggerUpdate() {
-    chrome.runtime.sendMessage({command: "update"});
+	chrome.runtime.sendMessage({ command: "update" });
 }
+
+//html files are not scanned for localization automatically.
+function updatePopupLocalizations() {
+	let reloadElement = document.getElementById("reload-button")
+	reloadElement.setAttribute("title", chrome.i18n.getMessage("reloadTitle"))
+
+	let readAllElement = document.getElementById("read-all-button")
+	readAllElement.setAttribute("title", chrome.i18n.getMessage("markAllAsReadTitle"))
+
+
+	//not strictly necessary as not shown, but good practice I think.
+	let titleElement = document.getElementById("title")
+	titleElement.innerHTML = chrome.i18n.getMessage("extName")
+
+}
+
